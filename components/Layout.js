54d5eae4c4f21f1ba4fb9644c0402b4e28ed50a9
Sulash Manandhar <sulash.manandhar@gmail.com>
2022-11-01 12:34:26 +0545

    Next JS basic network setup

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
+import Meta from "./Meta";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Meta />
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
